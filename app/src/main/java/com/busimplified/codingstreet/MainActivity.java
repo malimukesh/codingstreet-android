799f011699816904a402aec6ded7be07f2f8ec02
Mali Mukesh <malimukeshnitc@gmail.com>
2017-01-01 18:15:44 +0530

    Initial code setup

---
+++ app/src/main/java/com/busimplified/codingstreet/MainActivity.java
@@ -0,0 +1,72 @@
+package com.busimplified.codingstreet;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        boolean isConnected = isNetConnected(this);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.clearCache(true);
+        mAdView = (AdView) findViewById(R.id.adView);
+        if (isConnected) {
+            webView.loadUrl("file:///android_asset/connecting_website.html");
+            webView.loadUrl(String.valueOf(Uri.parse("http://codingstreet.com")));
+
+            AdRequest adRequest = new AdRequest.Builder().build();
+            if (adRequest != null) {
+                mAdView.loadAd(adRequest);
+            }
+
+        } else {
+            webView.loadUrl("file:///android_asset/connect_internet.html");
+        }
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        CookieManager.getInstance().setAcceptCookie(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+
+    }
+
+    public  boolean isNetConnected(Context context) {
+        if (context != null) {
+            final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context
+                    .CONNECTIVITY_SERVICE);
+            final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
